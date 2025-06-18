9da2c37047fc9207a37001efcaa30315a688ad8a
ysb5397 <ysb5397@gmail.com>
2025-06-18 16:04:43 +0900

    sdasd

---
+++ src/main/java/com/tenco/blog/model/Board.java
@@ -0,0 +1,21 @@
+package com.tenco.blog.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Table(name = "board_tb")
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private String content;
+    private String username;
+    private Timestamp createdAt;
+}
