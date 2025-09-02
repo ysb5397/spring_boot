fa981b7ed765edf88ee8555051d2bc582f39eb07
ysb5397 <ysb5397@gmail.com>
2025-09-02 09:20:40 +0900

    init

---
+++ src/main/java/com/example/class_step_websocket/ClassStepWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.class_step_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassStepWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassStepWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/class_step_websocket/ClassStepWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.class_step_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassStepWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
