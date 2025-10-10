5d4ba73375946763f558aa2ee6221780fe664c97
ysb5397 <ysb5397@gmail.com>
2025-10-10 10:17:30 +0900

    init

---
+++ src/main/java/com/puzzlix/solid_task/SolidTaskApplication.java
@@ -0,0 +1,13 @@
+package com.puzzlix.solid_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puzzlix/solid_task/SolidTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puzzlix.solid_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
